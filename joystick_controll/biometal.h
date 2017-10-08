472f1c3f8f78bfd2bf9cf7c3959733e68480023c
Satoru Osawa <s.osawa29@gmail.com>
2017-10-08 22:49:04 +0900

    it works

--- joystick_controll/biometal.h
+++ joystick_controll/biometal.h
@@ -6,7 +6,8 @@ class Biometal {
   Biometal();
   void setupBio();
   void updateBio(long diff);
-  void set(int value, long last_diff);
+  void setPwm(int value, long last_diff);
+  void setDeg(int deg_target, long last_diff);
   long heatParam() {
     return heat_param_;
   }
@@ -23,8 +24,8 @@ class Biometal {
 
 Biometal::Biometal() {
   heat_param_ = 0l;
-  heat_param_limit_ = 70l * 1000l * 255l;
   pwm_keep_ = 20;
+  heat_param_limit_ = 70l * 1000l * long(255 - pwm_keep_);
   pwm_ = 0;
 }
 
@@ -32,20 +33,33 @@ void Biometal::setupBio() {
   
 }
 
-
 void Biometal::updateBio(long diff) {
   heat_param_ += long(pwm_ - pwm_keep_) * diff;
-  if(heat_param_ < 0) {
+  if (heat_param_ < 0) {
     heat_param_ = 0;
   }
 }
 
-void Biometal::set(int value, long last_diff) {
+void Biometal::setPwm(int value, long last_diff) {
   long estimate_heat_param = heat_param_ + long(value - pwm_keep_) * last_diff;
-  if(estimate_heat_param > heat_param_limit_) {
+  if (estimate_heat_param > heat_param_limit_) {
     pwm_ = pwm_keep_;
   } else {
     pwm_ = value;
   }
 }
+
+void Biometal::setDeg(int deg_target, long last_diff) {
+  int deg = int(heat_param_ / (1000l * 255l));
+  int deg_diff = deg_target - deg;
+  int pwm = map(deg_diff, 5, 70, pwm_keep_, 550);
+  
+//  Serial.println(heat_param_diff);
+  Serial.println(deg_diff);
+
+  pwm = constrain(int(pwm), 0, 255);
+  setPwm(pwm, last_diff);
+}
+
+
 
--- joystick_controll/joystick_controll.ino
+++ joystick_controll/joystick_controll.ino
@@ -16,9 +16,13 @@ Biometal BIOMETAL = Biometal();
 
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
-//  pinMode(BIOMETAL_PIN1, OUTPUT);
-//  pinMode(BIOMETAL_PIN2, OUTPUT);
-//  pinMode(BIOMETAL_PIN3, OUTPUT);
+  pinMode(BIOMETAL_PIN1, OUTPUT);
+  pinMode(BIOMETAL_PIN2, OUTPUT);
+  pinMode(BIOMETAL_PIN3, OUTPUT);
+  analogWrite(BIOMETAL_PIN1, OFF_VALUE);  
+  analogWrite(BIOMETAL_PIN2, OFF_VALUE);  
+  analogWrite(BIOMETAL_PIN3, OFF_VALUE);  
+
 
   Serial.begin(115200);
   TICK.setupTick();
@@ -33,14 +37,15 @@ void loop() {
   TICK.updateTick();
   BIOMETAL.updateBio(TICK.diffMicros());
 
-  
   int joystic_x = analogRead(JOYSTIC_X);
 
-  joystic_x = map(joystic_x, 500, 1000, 0, 255);
-  joystic_x = constrain(joystic_x, 0, 255);
+//  joystic_x = map(joystic_x, 500, 1020, 0, 255);
+//  joystic_x = constrain(joystic_x, 0, 255);
+//  BIOMETAL.setPwm(joystic_x, TICK.diffMicros());
 
-  BIOMETAL.set(joystic_x, TICK.diffMicros());
-
+  joystic_x = map(joystic_x, 500, 1020, 0, 70);
+  joystic_x = constrain(joystic_x, 0, 70);
+  BIOMETAL.setDeg(joystic_x, TICK.diffMicros());
 
   analogWrite(BIOMETAL_PIN1, 255 - BIOMETAL.pwm());
 
@@ -55,3 +60,4 @@ void loop() {
   Serial.print("  diff:");
   Serial.println(TICK.diffMicros());
 }
+
