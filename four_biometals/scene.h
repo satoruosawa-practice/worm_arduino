f361268b6e0fab80c18c80a81a5dae49b9429207
Satoru Osawa <s.osawa29@gmail.com>
2017-10-24 23:02:53 +0900

    arranged

--- four_biometals/scene.h
+++ four_biometals/scene.h
@@ -28,22 +28,56 @@ Scene::Scene(Ticker &ticker, BiometalContainer &biometals) {
 }
 
 void Scene::sequence() {
-  if(que(0l, 3000000l)) {
-    setBiometals(70, 0, 0, 0);
-  } else if(que(3000000l, 4000000l)) {
-    setBiometals(0, 70, 0, 0);
-  } else if(que(4000000l, 5000000l)) {
-    setBiometals(0, 0, 70, 0);
-  } else if(que(5000000l, 6000000l)) {
-    setBiometals(0, 0, 0, 70);
+  // 0
+  if(que(0l, 118500000l)) {
+    if((ticker_->ellapsedMicros() / 1000000l)%14 == 0) {  // 14sec
+     setBio(0, 70);
+    } else if((ticker_->ellapsedMicros() / 1000000l)%14 == 7) {
+     setBio(0, 50);
+    } else {
+      setBio(0, 0);
+    }
   } else {
-    setBiometals(0, 0, 0, 0);
+    setBio(0, 0);
   }
+  // 1
+  if(que(0l, 118500000l)) {
+    if((ticker_->ellapsedMicros() / 100000l)%100 == 0) {
+     setBio(1, 70);
+    } else if((ticker_->ellapsedMicros() / 100000l)%100 == 50) {
+     setBio(1, 50);
+    } else {
+      setBio(1, 0);
+    }
+  } else {
+    setBio(1, 0);
+  }
+  // 2
+  if(que(0l, 118500000l)) {
+    if((ticker_->ellapsedMicros() / 2250000l)%2 == 0) {
+     setBio(2, 70);
+    } else {
+      setBio(2, 0);
+    }
+  } else {
+    setBio(2, 0);
+  }
+  // 3
+  if(que(0l, 118500000l)) {
+    if((ticker_->ellapsedMicros() / 2000000l)%2 == 0) {
+     setBio(3, 70);
+    } else {
+      setBio(3, 0);
+    }
+  } else {
+    setBio(3, 0);
+  }
+
 }
 
 bool Scene::que(long from, long to) {
   long now = ticker_->ellapsedMicros() - start_time_;
-  if(from < now && now < to) {
+  if(from < now && now <= to) {
     return true;
   } else {
     return false;
